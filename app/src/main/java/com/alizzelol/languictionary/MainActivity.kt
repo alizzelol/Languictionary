b23b504d763c705a124861a18c5ae4b16cb14cf2
alizzelol <alizzelol@gmail.com>
2025-06-05 12:37:57 +0200

    Initial commit

---
+++ app/src/main/java/com/alizzelol/languictionary/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.alizzelol.languictionary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.alizzelol.languictionary.ui.theme.LanguictionaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyView()
+        }
+    }
+}
+
+@Composable
+fun MyView(){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = stringResource(id = R.string.title), fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id = R.string.subtitle))
+    }
+}
