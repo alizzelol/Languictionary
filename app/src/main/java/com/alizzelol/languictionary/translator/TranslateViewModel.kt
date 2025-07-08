5799e499209b395de542f7a31a1902c70a6e35d8
alizzelol <alizzelol@gmail.com>
2025-07-08 20:03:20 +0200

    TranslateViewModel

--- app/src/main/java/com/alizzelol/languictionary/translator/TranslateViewModel.kt
+++ app/src/main/java/com/alizzelol/languictionary/translator/TranslateViewModel.kt
@@ -1,6 +1,12 @@
 package com.alizzelol.languictionary.translator
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TranslateViewModel : ViewModel() {
+
+    var state by mutableStateOf(TranslateState())
+
 }
