853f387fd846a6dea3b18b730efa44c9b19915fa
alizzelol <alizzelol@gmail.com>
2025-07-15 16:47:09 +0200

    DropDownLang

--- app/src/main/java/com/alizzelol/languictionary/translator/TranslateViewModel.kt
+++ app/src/main/java/com/alizzelol/languictionary/translator/TranslateViewModel.kt
@@ -1,10 +1,12 @@
 package com.alizzelol.languictionary.translator
 
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.Translator
@@ -36,13 +38,36 @@ class TranslateViewModel : ViewModel() {
                     translateText = translateText
                 )
             }.addOnFailureListener{
-
+                Toast.makeText(context, "Descargando modelo...", Toast.LENGTH_SHORT).show()
+                downloadingModel(languageTranslator, context)
             }
     }
 
     private fun downloadingModel(languageTranslator: Translator, context: Context){
+        state = state.copy(
+            isDownloading = true
+        )
+        val conditions = DownloadConditions
+            .Builder()
+            .requireWifi()
+            .build()
 
+        languageTranslator
+            .downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                Toast.makeText(context, "Modelo descargado correctamente", Toast.LENGTH_SHORT).show()
+                state = state.copy(
+                    isDownloading = false
+                )
+            }.addOnFailureListener{
+                Toast.makeText(context, "Error en la descarga del modelo", Toast.LENGTH_SHORT).show()
+            }
     }
+
+
+
+
+
 
 
 }
