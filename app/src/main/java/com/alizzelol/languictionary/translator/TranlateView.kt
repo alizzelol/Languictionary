70b720e9b100bbd07d08d120ac3f151bdeac5fd8
alizzelol <alizzelol@gmail.com>
2025-07-16 20:24:42 +0200

    Traduce

--- app/src/main/java/com/alizzelol/languictionary/translator/TranlateView.kt
+++ app/src/main/java/com/alizzelol/languictionary/translator/TranlateView.kt
@@ -1,8 +1,24 @@
 package com.alizzelol.languictionary.translator
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -11,8 +27,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -28,9 +46,92 @@ fun TranslateView(viewModel: TranslateViewModel){
     var expandSource by remember { mutableStateOf(false) }
     var expandTarget by remember { mutableStateOf(false) }
 
-    Column(modifier = Modifier.fillMaxSize()
+    var selectedSourceLang by remember{ mutableStateOf(languageOptions[0])}
+    var selectedTargetLang by remember{ mutableStateOf(languageOptions[1])}
+
+    Column(modifier = Modifier
+        .fillMaxSize()
         .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {  }
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically){
+
+            DropdownLang(
+                itemSelection = itemsSelection,
+                selectedIndex = indexSource,
+                expand = expandSource,
+                onClickExpanded = { expandSource = true},
+                onClickDismiss = { expandSource = false},
+                onClickItem = { index ->
+                    indexSource = index
+                    selectedSourceLang = languageOptions[index]
+                    expandSource = false
+                }
+            )
+
+            Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "",
+                modifier = Modifier.padding(start = 15.dp, end = 15.dp)
+                )
+
+            DropdownLang(
+                itemSelection = itemsSelection,
+                selectedIndex = indexTarget,
+                expand = expandTarget,
+                onClickExpanded = { expandTarget = true},
+                onClickDismiss = { expandTarget = false},
+                onClickItem = { index ->
+                    indexTarget = index
+                    selectedTargetLang = languageOptions[index]
+                    expandTarget = false
+                }
+            )
+
+        }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = state.textToTranslate,
+            onValueChange = {viewModel.onValue(it)},
+            label = {Text(text="Escribe...")},
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    viewModel.onTranslate(
+                        state.textToTranslate,
+                        context,
+                        selectedSourceLang,
+                        selectedTargetLang
+                    )
+                }
+            ),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Transparent,
+                unfocusedBorderColor = Color.Transparent
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        if(state.isDownloading){
+            CircularProgressIndicator()
+            Text(text = "Descargando...")
+        } else {
+            OutlinedTextField(
+                value = state.translateText,
+                onValueChange = {},
+                label = {Text(text="Tu texto traducido...")},
+                readOnly = false,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                ),
+                modifier = Modifier.fillMaxWidth().fillMaxHeight()
+            )
+        }
+
+
+
+    }
 
 }
